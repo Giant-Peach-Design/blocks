123615ad28d03bb6c3eb2edb39d2059583bdfcf9
Tom Gurney <6736496+tom-gurney@users.noreply.github.com>
2025-07-22 11:47:16 +0100

    Add fields.json support and key generation for ACF fields

--- src/Cli/Cli.php
+++ src/Cli/Cli.php
@@ -5,87 +5,84 @@ namespace Giantpeach\Schnapps\Blocks\Cli;
 class Cli
 {
 
-  public function __construct()
-  {
-    add_action('cli_init', [$this, 'registerCommands']);
-  }
-
-  public function registerCommands()
-  {
-    \WP_CLI::add_command('hello', [$this, 'hello']);
-    /**
-     * Register the create-block command
-     * 
-     * Example usage:
-     * wp create-block my-block - Create a new block
-     */
-    \WP_CLI::add_command('create-block', [$this, 'createBlock']);
-  }
-
-  public function createBlock($args, $assocArgs)
-  {
-    if (count($args) < 1) {
-      \WP_CLI::error('Please provide a block name');
+    public function __construct()
+    {
+        add_action('cli_init', [$this, 'registerCommands']);
     }
 
-    \WP_CLI::success('Creating block ' . $args[0]);
+    public function registerCommands()
+    {
+        \WP_CLI::add_command('hello', [$this, 'hello']);
+        /**
+         * Register the create-block command
+         *
+         * Example usage:
+         * wp create-block my-block - Create a new block
+         */
+        \WP_CLI::add_command('create-block', [$this, 'createBlock']);
+    }
 
-    $blockName = $args[0];
-    $blockName = str_replace(' ', '-', $blockName);
-    $blockName = strtolower($blockName);
-    $blockName = 'giantpeach/' . $blockName;
-    $className = ucfirst($args[0]);
-    $variableName = "$" . lcfirst($args[0]);
+    public function createBlock($args, $assocArgs)
+    {
+        if (count($args) < 1) {
+            \WP_CLI::error('Please provide a block name');
+        }
 
-    $traits = [];
-    $use = [
-      "use Giantpeach\Schnapps\Blocks\Block;"
-    ];
+        \WP_CLI::success('Creating block ' . $args[0]);
 
-    $blockPath = get_template_directory() . '/src/Blocks/' . $args[0];
-    $blockTemplatePath = $blockPath . '/view.twig';
-    $blockJsonPath = $blockPath . '/block.json';
-    $blockClassPath = $blockPath . '/' . $args[0] . '.php';
+        $blockName = $args[0];
+        $blockName = str_replace(' ', '-', $blockName);
+        $blockName = strtolower($blockName);
+        $blockName = 'giantpeach/' . $blockName;
+        $className = ucfirst($args[0]);
+        $variableName = "$" . lcfirst($args[0]);
 
-    $use = array_unique($use);
-    $use = implode("\n", $use);
+        $traits = [];
+        $use = [
+            "use Giantpeach\Schnapps\Blocks\Block;"
+        ];
 
-    if (count($traits) > 0) {
-      $traits = array_unique($traits);
-      $traits = "use " . implode(", ", $traits) . ";
+        $blockPath = get_template_directory() . '/src/Blocks/' . $args[0];
+        $blockTemplatePath = $blockPath . '/view.twig';
+        $blockJsonPath = $blockPath . '/block.json';
+        $blockClassPath = $blockPath . '/' . $args[0] . '.php';
+        $fieldsJsonPath = $blockPath . '/fields.json';
+
+        $use = array_unique($use);
+        $use = implode("\n", $use);
+
+        if (count($traits) > 0) {
+            $traits = array_unique($traits);
+            $traits = "use " . implode(", ", $traits) . ";
       ";
-    } else {
-      $traits = "";
-    }
+        } else {
+            $traits = "";
+        }
 
-    $renderCallback = sprintf("\\\Giantpeach\\\Schnapps\\\Theme\\\Blocks\\\Blocks::renderBlock");
+        $renderCallback = sprintf("\\\Giantpeach\\\Schnapps\\\Theme\\\Blocks\\\Blocks::renderBlock");
 
-    if (!file_exists($blockPath)) {
-      mkdir($blockPath);
-    }
+        if (!file_exists($blockPath)) {
+            mkdir($blockPath);
+        }
 
-    if (!file_exists($blockTemplatePath)) {
-      $template = <<<EOT
+        if (!file_exists($blockTemplatePath)) {
+            $template = <<<EOT
       <section class="{{ wrapperClass }}">
         <div class="content">
-          <InnerBlocks 
-            className="" 
-            allowedBlocks="{{ allowedBlocks | wp_json_encode }}"
-            template="{{ template | wp_json_encode }}" 
-          />
+
         </div>
       </section>
       EOT;
 
-      file_put_contents($blockTemplatePath, $template);
+            file_put_contents($blockTemplatePath, $template);
 
-      \WP_CLI::success('Template created');
-    } else {
-      \WP_CLI::error('Template already exists');
-    }
+            \WP_CLI::success('Template created');
+        } else {
+            \WP_CLI::error('Template already exists');
+        }
 
-    if (!file_exists($blockJsonPath)) {
-      $json = <<<EOT
+        if (!file_exists($blockJsonPath)) {
+            $json = <<<EOT
       {
         "name": "$blockName",
         "title": "$className",
@@ -104,19 +101,19 @@ class Cli
       }
       EOT;
 
-      file_put_contents($blockJsonPath, $json);
+            file_put_contents($blockJsonPath, $json);
 
-      \WP_CLI::success('Block JSON created');
-    } else {
-      \WP_CLI::error('Block JSON already exists');
-    }
+            \WP_CLI::success('Block JSON created');
+        } else {
+            \WP_CLI::error('Block JSON already exists');
+        }
 
-    if (!file_exists($blockClassPath)) {
-      $class = <<<EOT
+        if (!file_exists($blockClassPath)) {
+            $class = <<<EOT
       <?php
-      
+
       namespace Giantpeach\Schnapps\Theme\Blocks\\$className;
-      
+
       $use
 
       class $className extends Block
@@ -127,19 +124,60 @@ class Cli
       }
       EOT;
 
-      file_put_contents($blockClassPath, $class);
+            file_put_contents($blockClassPath, $class);
 
-      \WP_CLI::success('Block class created');
-    } else {
-      \WP_CLI::error('Block class already exists');
+            \WP_CLI::success('Block class created');
+        } else {
+            \WP_CLI::error('Block class already exists');
+        }
+
+        if (!file_exists($fieldsJsonPath)) {
+            $fieldsJson = <<<EOT
+      {
+        "title": "$className Block",
+        "fields": [
+          {
+            "label": "Title",
+            "name": "title",
+            "type": "text",
+            "instructions": "",
+            "required": 0,
+            "conditional_logic": 0,
+            "wrapper": {
+              "width": "",
+              "class": "",
+              "id": ""
+            },
+            "default_value": "",
+            "placeholder": "",
+            "prepend": "",
+            "append": "",
+            "maxlength": ""
+          }
+        ],
+        "menu_order": 0,
+        "position": "normal",
+        "style": "default",
+        "label_placement": "top",
+        "instruction_placement": "label",
+        "active": true,
+        "description": ""
+      }
+      EOT;
+
+            file_put_contents($fieldsJsonPath, $fieldsJson);
+
+            \WP_CLI::success('Fields JSON created');
+        } else {
+            \WP_CLI::error('Fields JSON already exists');
+        }
+
+        \WP_CLI::success('Block created');
+        //\WP_CLI::success('Don\'t forget to register the block in src/Blocks/Blocks.php');
     }
 
-    \WP_CLI::success('Block created');
-    //\WP_CLI::success('Don\'t forget to register the block in src/Blocks/Blocks.php');
-  }
-
-  public function hello()
-  {
-    \WP_CLI::line('Hello Worldz');
-  }
+    public function hello()
+    {
+        \WP_CLI::line('Hello Worldz');
+    }
 }
--- src/Fields.php
+++ src/Fields.php
@@ -4,7 +4,9 @@ namespace Giantpeach\Schnapps\Blocks;
 
 class Fields {
   public static function load(string $dir) {
+    $blockName = static::getBlockNameFromDir($dir);
 
+    // Try loading from PHP file first
     if (file_exists($dir . '/fields.php')) {
       $fields = require $dir . '/fields.php';
 
@@ -13,8 +15,119 @@ class Fields {
           return;
         }
 
+        // Set block location if not already set
+        $fields = static::setBlockLocation($fields, $blockName);
+
+        acf_add_local_field_group($fields);
+        return;
+      }
+    }
+
+    // Try loading from JSON file if PHP file doesn't exist
+    if (file_exists($dir . '/fields.json')) {
+      $jsonContent = file_get_contents($dir . '/fields.json');
+      $fields = json_decode($jsonContent, true);
+
+      if (is_array($fields) && json_last_error() === JSON_ERROR_NONE) {
+        if (!function_exists('acf_add_local_field_group')) {
+          return;
+        }
+
+        // Generate keys for the field group and all fields if they don't exist
+        $fields = static::generateKeys($fields);
+
+        // Set block location if not already set
+        $fields = static::setBlockLocation($fields, $blockName);
+
         acf_add_local_field_group($fields);
       }
     }
+  }
+
+  /**
+   * Generate ACF keys for field groups and fields that don't have them
+   */
+  private static function generateKeys(array $fields): array {
+    // Generate key for the field group if it doesn't exist
+    if (!isset($fields['key'])) {
+      $fields['key'] = 'group_' . uniqid();
+    }
+
+    // Generate keys for fields
+    if (isset($fields['fields']) && is_array($fields['fields'])) {
+      $fields['fields'] = static::generateFieldKeys($fields['fields']);
+    }
+
+    return $fields;
+  }
+
+  /**
+   * Recursively generate keys for fields and sub-fields
+   */
+  private static function generateFieldKeys(array $fields): array {
+    foreach ($fields as &$field) {
+      if (!isset($field['key'])) {
+        $field['key'] = 'field_' . uniqid();
+      }
+
+      // Handle repeater/flexible content sub-fields
+      if (isset($field['sub_fields']) && is_array($field['sub_fields'])) {
+        $field['sub_fields'] = static::generateFieldKeys($field['sub_fields']);
+      }
+
+      // Handle flexible content layouts
+      if (isset($field['layouts']) && is_array($field['layouts'])) {
+        foreach ($field['layouts'] as &$layout) {
+          if (!isset($layout['key'])) {
+            $layout['key'] = 'layout_' . uniqid();
+          }
+          if (isset($layout['sub_fields']) && is_array($layout['sub_fields'])) {
+            $layout['sub_fields'] = static::generateFieldKeys($layout['sub_fields']);
+          }
+        }
+      }
+
+      // Handle group sub-fields
+      if ($field['type'] === 'group' && isset($field['sub_fields']) && is_array($field['sub_fields'])) {
+        $field['sub_fields'] = static::generateFieldKeys($field['sub_fields']);
+      }
+    }
+
+    return $fields;
+  }
+
+  /**
+   * Get block name from directory path
+   */
+  private static function getBlockNameFromDir(string $dir): string {
+    // Extract block name from directory path
+    // e.g. /path/to/blocks/Hero -> Hero
+    $blockName = basename($dir);
+    
+    // Convert PascalCase to kebab-case for ACF block names
+    // e.g. TextImage -> text-image
+    $blockName = strtolower(preg_replace('/(?<!^)[A-Z]/', '-$0', $blockName));
+    
+    return 'giantpeach/' . $blockName;
+  }
+
+  /**
+   * Set the block location for the field group
+   */
+  private static function setBlockLocation(array $fields, string $blockName): array {
+    // Only set location if it doesn't already exist
+    if (!isset($fields['location']) || empty($fields['location'])) {
+      $fields['location'] = [
+        [
+          [
+            'param' => 'block',
+            'operator' => '==',
+            'value' => $blockName
+          ]
+        ]
+      ];
+    }
+
+    return $fields;
   }
 }
